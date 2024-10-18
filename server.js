8b9b72329ff5239c0ab852a5114d6e39069126ef
neeva124 <neevanaveenc@gmail.com>
2024-10-18 19:42:20 -0400

    Updated CSS file path and fixed static file serving

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname)));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/ripleys.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
