0b87bb64b398852c88cf684ad31d423fe15a7dfd
neeva124 <neevanaveenc@gmail.com>
2024-10-30 01:48:13 -0400

    bootstrap added in nodemodules,vercel success

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+// Get __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Serve static files (CSS, images, etc.)
-app.use('/images', express.static('/public/css'));
-app.use(express.static('public'));
+app.use('/images', express.static(path.join(__dirname, 'public/css')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
 // Route to serve the HTML file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/ripleys.html'));
+  res.sendFile(path.join(__dirname, 'public', 'ripleys.html'));
 });
 
 const PORT = 3000;
